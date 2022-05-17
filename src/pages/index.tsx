ef2a7a1e1b8ecdf4e31ace595c4187c530bc4ed7
Andrew Strigaliov <strigaliov4working@gmail.com>
2022-05-17 19:24:26 +0300

    Added husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center mt-3">
       <div className="text-3xl">
-        Tailwind - <span className="text-teal-500">ON</span>
+        Tailwind - <span className="text-teal-400">ON</span>
+      </div>
+      <div className="text-3xl">
+        Eslint - <span className="text-yellow-400">ON</span>
+      </div>
+      <div className="text-3xl">
+        Husky - <span className="text-red-400">ON</span>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
